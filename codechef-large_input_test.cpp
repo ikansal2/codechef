b4598c1a7af10a17482f45fe00a47ea4eea6ed20
ikansal2 <ishaankansal@Ishaans-MacBook-Pro.local>
2015-06-26 20:45:30 +0530

    code

--- codechef-large_input_test.cpp
+++ codechef-large_input_test.cpp
@@ -2,3 +2,19 @@
 Input test for large number of inputs
 */
 
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+ 
+int main()
+{
+    long int n,k,d;
+    int count=0;
+    cin>>n>>k;
+    for(int i=0;i<n;i++)
+    {
+        scanf("%ld",&d);
+        if(d%k==0) count++;
+    }
+    cout<<count<<endl;
+}
