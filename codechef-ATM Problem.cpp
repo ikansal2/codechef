7882b2b44303e4f17a13addb92c70d5789822b47
Ishaan Kansal <ishaankansal12@gmail.com>
2015-06-26 17:58:44 +0530

    Update codechef-ATM Problem.cpp

--- codechef-ATM Problem.cpp
+++ codechef-ATM Problem.cpp
@@ -1,9 +1,23 @@
 /*
 Codechef Problem- ATM, withdrawal from ATM account with 0.5 fixed charge on every transaction. Each wothdrawal has to be a multiple of 5
 
-eg 
-Input-11,12,88,42
-Output- 11,12,88
+Example - Successful Transaction
+Input:
+30 120.00
+Output:
+89.50
+
+Example - Incorrect Withdrawal Amount (not multiple of 5)
+Input:
+42 120.00
+Output:
+120.00
+
+Example - Insufficient Funds
+Input:
+300 120.00
+Output:
+120.00
 
 @author-ishaankansal
 Category-easy
